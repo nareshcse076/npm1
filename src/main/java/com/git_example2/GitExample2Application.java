85eda7c8fb6947b6cf33f75d819cbfc76b4069cf
nareshdemo <npmcse076@gmail.com>
2024-09-22 11:32:41 +0530

    Defect  3457-fixed the links that were not working in home page

---
+++ src/main/java/com/git_example2/GitExample2Application.java
@@ -0,0 +1,13 @@
+package com.git_example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExample2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_example2/GitExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_example2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
